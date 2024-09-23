72ab9027afbbbe46fc7e0ad58f9e4e97b21e0759
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-23 19:55:16 -0400

    Se puso como llave a registroId

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -1,5 +1,10 @@
-﻿namespace CarlosCustodio_Ap1_P1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarlosCustodio_Ap1_P1.Models;
 
 public class Registro
 {
+    [Key]
+    public int registroId { get; set; }
+    //[Required(ErrorMessage = "El campo es obligatorio")]
 }
