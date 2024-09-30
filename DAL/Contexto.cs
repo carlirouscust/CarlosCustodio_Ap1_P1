9a08f024cf8e0478c1c6458426c9fc9f1f464e27
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-30 18:39:20 -0400

    Se modifico el modelo que estaba en registro a prestamos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,5 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options)
     : base(options) { }
 
-    public DbSet<Registro> Registro { get; set; }
+    public DbSet<Prestamos> Registro { get; set; }
 }
