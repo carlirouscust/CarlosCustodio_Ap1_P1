04d7bb830484ee6cebcf2c4d7a0f009de9d6ac62
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-09-30 18:38:57 -0400

    Se renombro el modelo a prestamos y se creo las prioridades del modelo prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace CarlosCustodio_Ap1_P1.Models;
+
+public class Prestamos
+{
+    [Key]
+    public int prestamosId { get; set; }
+
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? deudor { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? concepto { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public int? monto { get; set; }
+}
