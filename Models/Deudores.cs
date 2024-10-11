f5d8925290c9a007646d52d14c79534e44a37d12
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-11 00:22:20 -0400

    se creo el modelo deudores

--- Models/Deudores.cs
+++ Models/Deudores.cs
@@ -6,9 +6,8 @@ namespace CarlosCustodio_Ap1_P1.Models;
 public class Deudores
 {
     [Key]
-    public int DeidorId { get; set; }
+    public int deudorId { get; set; }
 
     [Required(ErrorMessage = "El campo es obligatorio")]
     public string? Nombres { get; set; }
-    [Required(ErrorMessage = "El campo es obligatorio")]
 }
