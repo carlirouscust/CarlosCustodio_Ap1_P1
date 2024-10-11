b15a90304288bdf4f8945804cbd164329a0272dc
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-11 00:22:10 -0400

    se creo el modelo cobros detalles

---
+++ Models/CobrosDetalles.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarlosCustodio_Ap1_P1.Models;
+
+public class CobrosDetalles
+{
+	[Key]
+	public int detalleId { get; set; }
+	public decimal valorCobrado { get; set; }
+
+	public Cobros? cobro { get; set; }
+
+	[ForeignKey("cobro")]
+	public int cobroId { get; set; }
+
+	public Prestamos? prestamos { get; set; }
+
+	[ForeignKey("prestamos")]
+	public int prestamoId { get; set; }
+}
