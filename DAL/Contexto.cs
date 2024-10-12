7e8fb096f7fab236659fd4bce2461ab1964a2230
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-11 23:27:06 -0400

    se cambion el model creating

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -23,37 +23,18 @@ public class Contexto : DbContext
 			{
 				deudorId = 1,
 				Nombres = "Carlos"
+			},
+			new Deudores
+			{
+				deudorId = 2,
+				Nombres = "Maria"
+			},
+			new Deudores
+			{
+				deudorId = 3,
+				Nombres = "Juancito"
 			}
 		);
 
-		modelBuilder.Entity<Prestamos>().HasData(
-			new Prestamos
-			{
-				prestamoId = 1,
-				deudorId = 1,
-				concepto = "Casa",
-				monto = 5000,
-				balance = 3000
-			}			
-		);
-		modelBuilder.Entity<Cobros>().HasData(
-				new Cobros
-				{
-					cobroId = 1,
-					fecha = new DateTime(2024, 10, 11),
-					deudorId = 1,
-					monto = 2000
-				}
-			);
-
-		modelBuilder.Entity<CobrosDetalles>().HasData(
-			new CobrosDetalles
-			{
-				detalleId = 1,
-				cobroId = 1,
-				prestamoId = 1,
-				valorCobrado = 1000
-			}
-		);
 	}
 }
