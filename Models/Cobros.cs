f26f9bb56e7741fc5cde37f2183aea0e26b9fcdb
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2024-10-12 03:01:52 -0400

    se modifico el modelo cobros

--- Models/Cobros.cs
+++ Models/Cobros.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CarlosCustodio_Ap1_P1.Models;
 
@@ -7,7 +8,7 @@ public class Cobros
 {
 	[Key]
 	public int cobroId { get; set; }
-	public DateTime fecha { get; set; }
+	public DateTime fecha { get; set; } = DateTime.Now;
 	public decimal monto { get; set; }
 	
 	public  Deudores?  deudores { get; set; }
